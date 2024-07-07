b236327164ad9fdf1d97d1271517c66fc8424480
Kyungwon Kang <lim30339@gmail.com>
2024-07-07 15:41:19 +0900

    Initialize project

---
+++ BookSearcher/BookSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSearcherApp.swift
+//  BookSearcher
+//
+//  Created by Kyungwon Kang on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BookSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
